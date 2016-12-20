62bbe94a0bd8e708ea2d29cc04a97260f0f7c84d
Unknown <fredian.pc@gmail.com>
2016-12-20 09:55:06 +0700

    API routes

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,11 +1,20 @@
 import * as express from 'express';
 import {TodoRoutes} from '../api/todo/routes/todo-routes';
+import {DevelopmentRoutes} from '../api/development/routes/development-routes';
+import {PropertiesRoutes} from '../api/properties/routes/properties-routes';
+import {NewsletterRoutes} from '../api/newsletter/routes/newsletter-routes';
+import {UserRoutes} from '../api/user/routes/user-routes';
+import {UserGroupRoutes} from '../api/user_group/routes/user_group-routes';
 
 
 export class Routes {
    static init(app: express.Application, router: express.Router) {
      TodoRoutes.init(router);
-     
+     DevelopmentRoutes.init(router);
+     PropertiesRoutes.init(router);
+     NewsletterRoutes.init(router);
+     UserRoutes.init(router);
+     UserGroupRoutes.init(router);
 
      app.use('/', router);
    }
