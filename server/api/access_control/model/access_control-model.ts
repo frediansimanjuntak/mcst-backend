a73bc783c7a9729df660689dec392bde023e319f
Unknown <fredian.pc@gmail.com>
2016-12-29 16:23:22 +0700

    fix model access control

--- server/api/access_control/model/access_control-model.ts
+++ server/api/access_control/model/access_control-model.ts
@@ -3,7 +3,9 @@ import * as mongoose from 'mongoose';
 var accesscontrolSchema = new mongoose.Schema({
 	card_number:{type: String, required: true, trim: true},
 	issued_to:{type: String, trim: true},
-	unit_number:{type: String, trim: true},		
+	development:{type: String, trim: true},
+	properties:{type: String, trim: true},
+	access_type:{type: String, trim: true},		
 	access_level:{type: String, trim: true},
 	issued_by:{type: String, trim: true},
 	issued_on:{type: String, trim: true},
