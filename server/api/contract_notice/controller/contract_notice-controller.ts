d05776f96c20a379bd7213b8455fe0f9f2caa31d
Unknown <fredian.pc@gmail.com>
2017-01-17 17:12:09 +0700

    add api contract notice

---
+++ server/api/contract_notice/controller/contract_notice-controller.ts
@@ -0,0 +1,59 @@
+import * as express from 'express';
+import ContractDAO from '../../contract/dao/contract-dao';
+
+export class ContractNoticeController {
+  static getAllContractNotice(req: express.Request, res: express.Response):void {
+      let _id = req.params.id;
+
+      ContractDAO
+        ['getAllContractNotice'](_id)
+        .then(contractnotices => res.status(200).json(contractnotices))
+        .catch(error => res.status(400).json(error));
+  }
+
+  static getByIdContractNotice(req: express.Request, res: express.Response):void {
+      let _id = req.params.id;
+      let _idcontractnotice = req.params.idcontractnotice;
+
+      ContractDAO
+        ['getByIdContractNotice'](_id, _idcontractnotice)
+        .then(contractnotices => res.status(200).json(contractnotices))
+        .catch(error => res.status(400).json(error));
+  }
+
+  static createContractNotice(req: express.Request, res: express.Response):void {
+      let _id = req.params.id;
+      let _contractnotice = req.body;
+      _contractnotice.files=req.files;
+      let _userId = req.user._id;
+
+      ContractDAO
+        ['createContractNotice'](_id, _userId, _contractnotice)
+        .then(contract => res.status(201).json(contract))
+        .catch(error => res.status(400).json(error));
+  }
+
+  static deleteContractNotice(req: express.Request, res: express.Response):void {
+    let _id = req.params.id;
+    let _idcontractnotice = req.params.idcontractnotice;
+
+    ContractDAO
+      ['deleteContractNotice'](_id, _idcontractnotice)
+      .then(() => res.status(200).end())
+      .catch(error => res.status(400).json(error));
+  }
+
+  static updateContractNotice(req: express.Request, res: express.Response):void {
+    let _id = req.params.id;
+    let _idcontractnotice = req.params.idcontractnotice;
+    let _contractnotice = req.body;    
+    _contractnotice.files=req.files;
+    let _userId = req.user._id;
+
+    ContractDAO
+      ['updateContractNotice'](_id, _idcontractnotice, _userId, _contractnotice)
+      .then(contract => res.status(201).json(contract))
+      .catch(error => res.status(400).json(error));
+  }
+
+}
---
+++ server/api/contract_notice/routes/contract_notice-routes.ts
@@ -0,0 +1,23 @@
+"use strict";
+
+import * as express from 'express';
+import {ContractNoticeController} from '../controller/contract_notice-controller';
+import * as auth from '../../../auth/auth-service';
+
+export class ContractNoticeRoutes {
+    static init(router: express.Router) {
+      router
+        .route('/api/contract_notice/:id')
+        .get(auth.isAuthenticated(), ContractNoticeController.getAllContractNotice)
+        .post(auth.isAuthenticated(), ContractNoticeController.createContractNotice);
+
+      router
+        .route('/api/contract_notice/:id/:idcontractnote')
+        .get(auth.isAuthenticated(), ContractNoticeController.getByIdContractNotice)
+        .delete(auth.isAuthenticated(), ContractNoticeController.deleteContractNotice);
+
+      router
+        .route('/api/contract_notice/:id/update/:idcontractnote')
+        .post(auth.isAuthenticated(), ContractNoticeController.updateContractNotice);      
+    }
+}
