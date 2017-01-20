60ea0c4e09def80b638e1997d8954b75709dfb5e
Unknown <fredian.pc@gmail.com>
2017-01-20 11:15:13 +0700

    add api payment system

---
+++ server/api/payment_system/model/payment_system-model.ts
@@ -0,0 +1,28 @@
+import * as mongoose from 'mongoose';
+var Schema = mongoose.Schema;
+
+var paymentsystemSchema = new mongoose.Schema({
+	development:{
+		type: Schema.Types.ObjectId,
+    	ref: 'Development'
+	},
+	property:{type: String, required: true, trim: true},
+	title:{type: String, trim: true},
+	auto_issue_on:{type: String, trim: true},
+	due_on:{type: String, trim: true},
+	message_to_receiver:{type: String, trim: true},
+	notification_list:[{
+		charge:{type: String, trim: true},
+		remarks:{type: String, trim: true},
+		applies_to:{type: String, trim: true},
+		amount:{type: String, trim: true}
+	}],
+	publish:{type: String, trim: true, default:"false"},
+	created_by:{
+		type: Schema.Types.ObjectId,
+    	ref: 'User'
+	},
+	created_at:{type: Date, default: Date.now}
+});
+
+export default paymentsystemSchema;
