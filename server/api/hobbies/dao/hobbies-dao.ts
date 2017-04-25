72ee724f61924f269eedde74c60e26288ea27edd
Unknown <fredian.pc@gmail.com>
2017-04-25 15:35:39 +0700

    add api hobby

---
+++ server/api/hobbies/dao/hobbies-dao.ts
@@ -0,0 +1,110 @@
+import * as mongoose from 'mongoose';
+import * as Promise from 'bluebird';
+import * as _ from 'lodash';
+import hobbiesSchema from '../model/hobbies-model';
+import {GlobalService} from '../../../global/global.service';
+
+hobbiesSchema.static('getAll', ():Promise<any> => {
+    return new Promise((resolve:Function, reject:Function) => {
+        let _query = {};
+
+        Hobbies
+          .find(_query)
+          .exec((err, hobbies) => {
+              err ? reject(err)
+                  : resolve(hobbies);
+          });
+    });
+});
+
+hobbiesSchema.static('getById', (id:string):Promise<any> => {
+    return new Promise((resolve:Function, reject:Function) => {
+        if (!_.isString(id)) {
+            return reject(new TypeError('Id is not a valid string.'));
+        }
+
+        Hobbies
+          .findById(id)
+          .exec((err, hobbies) => {
+              err ? reject(err)
+                  : resolve(hobbies);
+          });
+    });
+});
+
+hobbiesSchema.static('createHobbies', (data:Object):Promise<any> => {
+    return new Promise((resolve:Function, reject:Function) => {
+        if (!_.isObject(data)) {
+          return reject(new TypeError('Access Control is not a valid object.'));
+        }
+
+        let body:any = data;
+        let hobbies = body.hobbies;
+
+        for(var i = 0; i <hobbies.length; i++){
+          let hobby = hobbies[i];
+          Hobbies
+            .find({"name": hobby})
+            .exec((err, res) => {
+              if(err){
+                reject(err);
+              }
+              if(res){
+                if(res.length == 0){
+                  var _hobby = new Hobbies();
+                  _hobby.name = hobby;
+                  _hobby.slug = GlobalService.slugify(hobby)
+                  _hobby.save((err, saved) => {
+                    err ? reject(err)
+                        : resolve(saved);
+                  });
+                }
+                if(res.length >= 1){
+                  resolve({message: "Already data"})
+                }
+              }
+            })
+        }        
+    });
+});
+
+hobbiesSchema.static('deleteHobbies', (data:Object):Promise<any> => {
+    return new Promise((resolve:Function, reject:Function) => {
+        let body:any = data;
+        let hobby = body.hobby;
+
+        Hobbies
+          .findOneAndRemove({"name": hobby})
+          .exec((err, deleted) => {
+              err ? reject(err)
+                  : resolve();
+          });
+    });
+});
+
+hobbiesSchema.static('updateHobbies', (id:string, data:Object):Promise<any> => {
+    return new Promise((resolve:Function, reject:Function) => {
+        if (!_.isString(id)) {
+            return reject(new TypeError('Id is not a valid string.'));
+        }
+        let body:any = data;
+        let hobby = body.hobby;
+        let slug = GlobalService.slugify(hobby)
+
+        Hobbies
+          .update({"_id": id}, {
+            $set: {
+              "name": hobby,
+              "slug": slug
+            }
+          })
+          .exec((err, updated) => {
+                err ? reject(err)
+                    : resolve(updated);
+          });
+    });
+});
+
+let Hobbies = mongoose.model('Hobbies', hobbiesSchema);
+
+export default Hobbies;
---
+++ server/api/hobbies/routes/hobbies-routes.ts
@@ -0,0 +1,23 @@
+"use strict";
+
+import * as express from 'express';
+import {HobbiesController} from '../controller/hobbies-controller';
+import * as auth from '../../../auth/auth-service';
+
+export class AccessControlRoutes {
+    static init(router: express.Router) {
+      router
+        .route('/hobbies')
+        .get(auth.isAuthenticated(), HobbiesController.getAll)
+        .post(auth.isAuthenticated(), HobbiesController.createHobbies);
+
+      router
+        .route('/hobbies/:id')
+        .get(auth.isAuthenticated(), HobbiesController.getById)
+        .delete(auth.isAuthenticated(), HobbiesController.deleteHobbies);
+
+      router
+        .route('/hobbies/update/:id')
+        .post(auth.isAuthenticated(), HobbiesController.updateHobbies);
+    }
+}
